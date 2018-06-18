08581f3f2bcdc1b96b0d8ec1996e2f87fa5f66e6
Vincent M. van der Meulen <meulen@uw.edu>
2018-06-17 21:56:49 -0700

    Finish lesson on testing shallow components

--- src/App.js
+++ src/App.js
@@ -4,55 +4,23 @@ import TodoList from "./TodoList";
 import logo from "./logo.svg";
 import "./App.css";
 
+const Test = () => <div>Testing</div>;
+
 class App extends Component {
-    state = {
-        on: false,
-        input: "",
-        mainColor: "blue",
-        lifeCycle: ""
-    };
-    handleStrings(str) {
-        if (str === "Hello World") return true;
-        return false;
-    }
-    componentDidMount() {
-        this.setState({ lifeCycle: "componentDidMount" });
-    }
-    componentWillReceiveProps() {
-        this.setState({ lifeCycle: "componentWillReceiveProps" });
-    }
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
-                    <h3 className={this.state.mainColor}>
-                        Everyone is Welcome!
-                    </h3>
                 </header>
-                <Form />
-                <p className="App-intro">Hello World</p>
-                <p className="button-state">{this.state.on ? "Yes!" : "No!"}</p>
-                <button onClick={() => this.setState({ on: true })}>
-                    Click
-                </button>
-                <h2>{this.state.input}</h2>
-                <input
-                    onChange={e =>
-                        this.setState({ input: e.currentTarget.value })
-                    }
-                    type="text"
-                />
-                <p className="lifeCycle">{this.state.lifeCycle}</p>*
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to
+                    reload.
+                </p>
+                <Test />
             </div>
         );
-    }
-}
-
-export class Link extends Component {
-    render() {
-        return this.props.hide ? null : <a href={this.props.address}>Click</a>;
     }
 }
 
---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("<App />", () => {
+    it("should render App", () => {
+        const wrapper = shallow(<App />, {
+            context: {},
+            disableLifecycleMethods: bool
+        });
+        console.log(wrapper.debug());
+    });
+});
